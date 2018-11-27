d53a6fcf3828ad89e5bc42dd3613d3d092b2e095
Wangdawei00 <wangdawei01@sjtu.edu.cn>
2018-11-27 13:57:07 +0800

    learning how to use sleep

--- main.cpp
+++ main.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
+#include <chrono>
 #include "vector.h"
 #include "Ticket.h"
 #include "Singleton.h"
 #include "Vehicle.h"
-#include <ctime>
+#include <thread>
+//#include <ctime>
 
 //#include <GL/glut.h>
 using namespace std;
 
 int main() {
+    this_thread::sleep_for(2min);
 //    cout<<Vehicle::a.find(1)->second<<endl;
-    Vehicle::setPrice();
-    Singleton &a=Singleton::getInstance();
+//    Vehicle::setPrice();
+//    Singleton &a=Singleton::getInstance();
+
     return 0;
 }
