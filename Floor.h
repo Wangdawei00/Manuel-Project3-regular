763d9dabac98b470df2dd875c1312299c10d09c6
Shenjiale2000 <45332839+Shenjiale2000@users.noreply.github.com>
2018-11-28 17:47:59 +0800

    Add files via upload

---
+++ Floor.h
@@ -0,0 +1,25 @@
+//
+// Created by starrynight on 2018/11/25.
+//
+
+#ifndef MANUEL_PROJECT3_MASTER_FLOOR_H
+#define MANUEL_PROJECT3_MASTER_FLOOR_H
+
+#include <iostream>
+#include "slot.h"
+#include <vector>
+using namespace std;
+class Floor {
+public:
+    Floor(int row,int column,string typeIn);
+
+    vector<int> &find_empty_slot();
+    void clear_slot(int rowIn,int columnIn);
+    vector<Slot> &operator[](int a);
+private:
+    int row,column;
+    vector<vector<Slot>> slots;
+    std::string type;
+};
+
+#endif //MANUEL_PROJECT3_MASTER_FLOOR_H
