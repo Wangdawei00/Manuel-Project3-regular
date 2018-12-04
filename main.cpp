9331408a3837818c300c12b96d55661bb41c6958
Wangdawei00 <wangdawei01@sjtu.edu.cn>
2018-12-04 11:41:11 +0800

    add main

--- main.cpp
+++ main.cpp
@@ -2,20 +2,25 @@
 #include "merge.h"
 #include "Ticket.h"
 #include <thread>
+#include <cstring>
 //#include <parkingLot.h>
 //#include <GL/glut.h>
 using namespace std;
 
 int main(int argc, char *argv[]) {
     string fileName;
-    if (argc > 1) {
-        fileName = argv[2];
+    int timePeriod = 0;
+    for (int i = 0; i < argc; ++i) {
+        if (strcmp(argv[i], "--file") == 0) {
+            fileName = argv[i + 1];
+        } else if (strcmp(argv[i], "--time") == 0) {
+            timePeriod = stoi(argv[i + 1], nullptr, 10);
+        }
     }
+    cout << timePeriod << endl;
     ParkingLot parkingLot(fileName);
     Vehicle::setPrice(fileName);
-    int timePeriod;
-    cout << "Please input a ";
-    cin >> timePeriod;
+
 //    cout << "Hello, World!" << endl;
     return 0;
 }
