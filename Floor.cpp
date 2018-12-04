8efff22078a9a65361604bd478df20215a4bb2a6
Shenjiale2000 <45332839+Shenjiale2000@users.noreply.github.com>
2018-12-04 11:02:57 +0800

    Add files via upload

--- Floor.cpp
+++ Floor.cpp
@@ -4,11 +4,12 @@
 
 #include "Floor.h"
 #include <iostream>
+#include <random>
+#include "Ticket.h"
 
 using namespace std;
 
-Floor::Floor(int rowIn, int columnIn, string typeIn,
-             int index) {//generate a new floor according to the rowIn,columnIn and typeIn
+Floor::Floor(int rowIn, int columnIn, string typeIn,int index) {//generate a new floor according to the rowIn,columnIn and typeIn
     Slot S = Slot(std::move(typeIn));//generate a new slot(a type defined in "slot.h") with the type of "typeIn"
     int i, j;
     row = rowIn;
@@ -16,18 +17,21 @@ Floor::Floor(int rowIn, int columnIn, string typeIn,
     vector<Slot> a;//generate a vector of Slot(a type defined in "slot.h")
     for (i = 0; i < column; ++i) {
         a.push_back(S);//add new element(a new slot "S")to the end of the vector "a"
+
     }
-    this->index = index;
+    this->index=index;
     for (j = 0; j < row; ++j) {
         slots.push_back(a);//add "a"to the end of the whole slots
     }
 }
 
-vector<int> const &Floor::find_empty_slot() const {
+vector<int> &Floor::find_empty_slot() {
+//    int (*empty)[2];
     for (int i = 0; i < row; i++) {
         vector<Slot> a = slots[i];//take out the "i" row of the whole "slots"
         for (int j = 0; j < column; j++) {
             if (a[j].Is_empty() == 1) {//if there exists an empty slot
+//                return a[i][j];
                 static vector<int> ret(2, 0);//generate a vector containing 2 integers
                 ret[0] = i;
                 ret[1] = j;
@@ -35,7 +39,7 @@ vector<int> const &Floor::find_empty_slot() const {
             }
         }
     }
-    static const vector<int> b = vector<int>(2, -1);//otherwise return (-1,-1) to indicate no empty slot
+    static vector<int> b = vector<int>(2, -1);//otherwise return (-1,-1) to indicate no empty slot
     return b;
 }
 
@@ -47,4 +51,29 @@ void Floor::clear_slot(int rowIn, int columnIn) {
 
 vector<Slot> &Floor::operator[](int a) {
     return slots[a];//return a single row "a" from "slots"
+}
+vector<int>& Floor::return_size() {
+    static vector<int> size(2,0);
+    size[0]=row;size[1]=column;
+    return size;
+}
+void refresh_Floor(Floor *F){ // this function is out of the class of Floor
+    for (int i=0;i<(*F).return_size()[0];i++){
+        for (int j=0;j<(*F).return_size()[1];j++){
+            if ((*F)[i][j].Is_empty()){ // jump the empty slots
+                continue;
+            }
+            else {
+                default_random_engine e;
+                uniform_real_distribution<double> u(0.0,100.0);
+                if (u(e)<=10.0){
+                    (*F)[i][j].Make_empty();
+                    DepartTicket D;
+                    D.set((*F)[i][j].return_vehicle().get_all_time(),(*F)[i][j].return_vehicle().getPrice(),
+                            (*F)[i][j].return_vehicle().getType());
+                    D.print();
+                }
+            }
+        }
+    }
 }
--- Floor.h
+++ Floor.h
@@ -8,24 +8,20 @@
 #include <iostream>
 #include "slot.h"
 #include <vector>
-
 using namespace std;
-
 class Floor {
 public:
-    Floor(int row, int column, string typeIn, int index);
+    Floor(int row,int column,string typeIn,int index);
 
-    vector<int> const &find_empty_slot() const;
-
-    void clear_slot(int rowIn, int columnIn);
-
+    vector<int> &find_empty_slot();
+    void clear_slot(int rowIn,int columnIn);
     vector<Slot> &operator[](int a);
-
+    vector<int> &return_size();
 private:
-    int row, column;
+    int row,column;
     vector<vector<Slot>> slots;
     std::string type;
     int index;
 };
-
+void refresh_Floor(Floor *F);
 #endif //MANUEL_PROJECT3_MASTER_FLOOR_H
